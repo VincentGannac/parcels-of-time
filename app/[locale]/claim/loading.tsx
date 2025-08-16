dfb80155936a32fd6d982880f178a68914a3e3a7
Vincent Gannac <vincentgannac@MacBook-Air-de-Vincent.local>
2025-08-16 17:02:19 +0200

    fix

---
+++ app/[locale]/claim/loading.tsx
@@ -0,0 +1,2 @@
+// optionnel si tu avais un loading dédié :
+export { default as loading } from '../../claim/loading'
--- app/[locale]/claim/page.tsx
+++ app/[locale]/claim/page.tsx
@@ -1,5 +1,3 @@
 // app/[locale]/claim/page.tsx
 export { default } from '../../claim/page'
 
-// optionnel si tu avais un loading dédié :
-export { default as loading } from '../../claim/loading'
