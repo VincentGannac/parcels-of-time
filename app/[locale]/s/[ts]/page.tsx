18c7a462f5db051c555928d31e7326bd5f2aadcb
Vincent Gannac <vincentgannac123@gmail.com>
2025-09-30 10:27:59 +0200

    fix

--- app/[locale]/s/[ts]/page.tsx
+++
@@ -1,7 +0,0 @@
-//app/[locale]/s/[ts]/page.tsx
-import { redirect } from 'next/navigation'
-type Params = { ts: string }
-export default async function Page({ params }: { params: Promise<Params> }) {
-  const { ts } = await params
-  redirect(`/m/${encodeURIComponent(ts)}`)
-}
