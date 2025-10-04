b27e2b087bbd4221933d7a74ebce0f2bb8400dac
Vincent Gannac <vincentgannac123@gmail.com>
2025-10-04 10:30:43 +0200

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ export const runtime = 'nodejs'
 export const dynamic = 'force-dynamic'
 
 import type { Metadata } from 'next'
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -15,7 +16,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body>{children}
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
